c7363f5c11d6f02794d0fa7700d0e9bf483e6197
AlexisGitHu <56341573+AlexisGitHu@users.noreply.github.com>
2020-10-30 11:19:11 +0100

    Add files via upload

--- Codigo/HelloWorld.py
+++ Codigo/HelloWorld.py
@@ -1,10 +1,158 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 14 20:04:49 2020
+Created on Fri Oct 16 16:42:00 2020
 
-@author: Alexis Gómez Chimeno
+@author: alexg
 """
 
-a = "Hello world"
+"""
+Vamos a hacer nuestro primer programa, que va a ser Hello World
 
+"""
+
+"""
+print("Hello world")
+"""
+
+"""
+Tipos:
+int -> número enteros
+float o double -> números reales
+char  -> caracteres alfanúmericos
+Strings -> cadena de caracteres 
+Boolean -> verdadero o falso
+"""
+
+"""
+Operadores:
+    básicos:
+    + -> suma
+    - ->resta
+    * -> multiplicación
+    ** -> exponenciación
+Operadores de comparación:
+    < -> menor que
+    > -> mayor que
+    >= -> mayor o igual que
+    <= -> menor o igual que
+
+"""
+
+"""
+Si mi madre se entera de que me he comido las galletas, me regaña
+Si mi madre se entera de que me he comido las galletas, me regañana, pero, sino me las he comido, me felicita
+elif -> else if
+"""
+"""
+Programa: si a es mayor que b y b es menor que c -> mostrar por pantalla la suma de todos a + b + c, 
+pero si a es mayor que b y b es mayor que c -> la resta de a - b -c
+En caso contrario -> muéstrame los tres números
+"""
+
+"""
+a=20
+b=20
+c=10
+
+
+if(a>b & b<c):
+    print(a+b+c)
+elif(a>b & b>c):
+    print(a-b-c)
+else:
+    print(a,b,c)
+"""
+
+"""
+a=10
+
+b=int(input("Introduce un número entero: "))
+
+if(a>b):
+    print(b)
+else:
+    print(a)
+"""
+
+"""
+lista =[10,"hola",True,20.4]
+"""
+
+"""
+Mostradme por pantalla los números del 1 al 100
+"""
+
+"""
+a=1
+(a)(++) -> post-incremento
+(++)(a) -> pre-incremento
+while(a<101):
+    print(a)
+    a=a+1
+    
+"""
+"""
+Función Range
+a=range(5)
+print(list(a))  
+"""
+
+
+"""
+a=10
+for i in range(5:2):
+    a=a+1
 print(a)
+"""
+"""
+a = range(6)
+lista = [2,3,4,5]
+"""
+"""
+[a,b) -> intervalo semicerrado 
+for i in range(0,52,2):
+    print(i)
+"""
+"""
+def suma(x,y):
+    return (x+y)
+    
+c=suma(3,2)
+print(c)    
+"""
+print(list(range(5)))
+
+lista=[2,5,8,10]
+"""
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
